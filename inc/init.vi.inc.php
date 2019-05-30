f1d32e9917a9010363b984fc37e80f7ab3a1396d
Phạm Tùng <tungpham@kambria.io>
2019-05-30 13:17:38 +0700

    remove redis

--- inc/init.vi.inc.php
+++ inc/init.vi.inc.php
@@ -1,6 +1,2 @@
 <?php
-//ini_set('session.save_handler','files');
-//ini_set('session.save_path','/tmp');
-ini_set('session.save_handler', 'redis');
-ini_set('session.save_path', 'tcp://127.0.0.1:6379');
 require realpath($_SERVER['DOCUMENT_ROOT']).'/inc/functions.vi.inc.php';
